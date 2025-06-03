341711b26175b9b8161323968605fedca054a6e6
Stuart <stuart_ringrose30@student.friends.tas.edu.au>
2025-06-03 04:09:57 +0000

    Changed eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,25 @@
-// @ts-check
-
+// eslint.config.mjs
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
+  // Base ESLint recommended config
   eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+
+  // TypeScript ESLint recommended config
+  ...tseslint.configs.recommended,
+
+  // Custom config object
+  {
+    rules: {
+      // Downgrade unused vars to warning
+      '@typescript-eslint/no-unused-vars': 'warn',
+
+      // Allow namespaces
+      '@typescript-eslint/no-namespace': 'off',
+
+      // Allow undefined globals like `console`
+      'no-undef': 'off',
+    },
+  },
+];
